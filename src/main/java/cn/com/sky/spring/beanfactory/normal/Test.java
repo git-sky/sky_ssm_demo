18e24cb08a0eb6d58d67c5f3bf666388f7b202ce
git-sky <linkme2008@sina.com>
2017-09-14 17:42:12 +0800

    add

--- src/main/java/cn/com/sky/spring/beanfactory/normal/Test.java
+++ src/main/java/cn/com/sky/spring/beanfactory/normal/Test.java
@@ -1,6 +1,7 @@
 package cn.com.sky.spring.beanfactory.normal;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
@@ -23,13 +24,25 @@ public class Test {
 		ResourcePatternResolver resourceLoader = new PathMatchingResourcePatternResolver();
 		// 2.Resource
 		Resource resource = resourceLoader.getResource(configLocation);
+		//
+		// XmlBeanFactory beanFactory = new XmlBeanFactory(resource);
 
-		XmlBeanFactory beanFactory = new XmlBeanFactory(resource);
+		XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(configLocation));
 
 		Student stu = (Student) beanFactory.getBean("sky");
 		System.out.println(stu);
-		Student stu2 = (Student) beanFactory.getBean("sky");
+
+		Student stu2 = (Student) beanFactory.getBean("sky2");
 		System.out.println(stu2);
+
+		Student stu3 = (Student) beanFactory.getBean("sky3");
+		System.out.println(stu3);
+
+		Student stu5 = (Student) beanFactory.getBean("sky5");
+		System.out.println(stu5);
+
+		Student stu6 = (Student) beanFactory.getBean("sky6");
+		System.out.println(stu6);
 
 	}
 }
---
+++ src/main/java/cn/com/sky/spring/custom/MyNamespaceHandler.java
@@ -0,0 +1,14 @@
+package cn.com.sky.spring.custom;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * registerBeanDefinitionParser("people", new
+ * PeopleBeanDefinitionParser());就是用来把节点名和解析类联系起来，在配置中引用people配置项时
+ * ，就会用PeopleBeanDefinitionParser来解析配置。
+ */
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+	public void init() {
+		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
+	}
+}
