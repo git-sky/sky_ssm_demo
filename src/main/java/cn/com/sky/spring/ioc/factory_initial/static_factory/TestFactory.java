2d013c97d6062eb53f5c12e123af6a9a3af5eaba
git-sky <linkme2008@sina.com>
2020-02-01 11:38:07 +0800

    update

--- src/main/java/cn/com/sky/spring/ioc/factory_initial/static_factory/TestFactory.java
+++ src/main/java/cn/com/sky/spring/ioc/factory_initial/static_factory/TestFactory.java
@@ -1,10 +1,12 @@
 package cn.com.sky.spring.ioc.factory_initial.static_factory;
 
+import cn.com.sky.spring.ioc.factory_initial.HelloApi;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import cn.com.sky.spring.ioc.factory_initial.HelloApi;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * <pre>
@@ -30,6 +32,23 @@ public class TestFactory {
 
         // 使用静态工厂方法
         BeanFactory beanFactory = new ClassPathXmlApplicationContext(configLocation);
+        HelloApi bean3 = beanFactory.getBean("bean3", HelloApi.class);
+        bean3.sayHello();
+        System.out.println(bean3);
+    }
+
+    @Test
+    public void testBeanFactory() {
+        String configLocation = getPath() + "/initial.xml";
+
+//        Resource resource = new ClassPathResource(configLocation);
+//        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+//        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+//        reader.loadBeanDefinitions(resource);
+//        factory.addBeanPostProcessor(new MyBeanPostProcessor());
+
+        ConfigurableBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(configLocation));
+
         HelloApi bean3 = beanFactory.getBean("bean3", HelloApi.class);
         bean3.sayHello();
         System.out.println(bean3);
